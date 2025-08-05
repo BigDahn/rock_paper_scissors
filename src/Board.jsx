a8274452a98ac48f0fc548ad3008bcf4343e671b
Big Dahn <harkinborholarholuwah@gmail.com>
2025-08-05 01:36:19 +0100

    board buttons shadow

--- src/Board.jsx
+++ src/Board.jsx
@@ -6,15 +6,15 @@ function Board() {
       <main className="grid grid-cols-1 justify-center place-items-center h-full w-full ">
         <section className="flex justify-between">
           <div className="w-[198px] h-[203px]  flex items-center justify-center cursor-pointer hover:shadow-2xl hover:transition-all hover:duration-100 hover:ease-linear rounded-full">
-            <div className="w-[152px] h-[152px] rounded-full bg-[#4664F4] px-6 flex items-center justify-center">
-              <div className="bg-gray-300 w-[152px] h-[105px] rounded-full flex items-center justify-center">
+            <div className="w-[152px] h-[152px] rounded-full bg-[#4664F4] px-6 flex items-center justify-center shadow-[inset_0_-6px_0_#2A45C2]">
+              <div className="bg-gray-300 w-[152px] h-[105px] rounded-full flex items-center justify-center shadow-[inset_0_6px_0_0_#BABFD4]">
                 <img src="/icon-paper.svg" />
               </div>
             </div>
           </div>
           <div className="w-[198px] h-[203px]  flex items-center justify-center cursor-pointer hover:shadow-2xl hover:transition-all hover:duration-100 hover:ease-linear rounded-full">
-            <div className="w-[152px] h-[152px]  bg-[#EB9F0E] px-6 rounded-full flex items-center justify-center">
-              <div className="bg-gray-300 w-[152px] h-[105px] rounded-full flex items-center justify-center">
+            <div className="w-[152px] h-[152px]  bg-[#EB9F0E] px-6 rounded-full flex items-center justify-center shadow-[inset_0_-6px_0_#C76C1B]">
+              <div className="bg-gray-300 w-[152px] h-[105px] rounded-full flex items-center justify-center shadow-[inset_0_6px_0_0_#BABFD4]">
                 <img src="/icon-scissors.svg" />
               </div>
             </div>
@@ -22,8 +22,8 @@ function Board() {
         </section>
         <main>
           <button className="w-[198px] h-[203px]  flex items-center justify-center cursor-pointer hover:shadow-2xl hover:transition-all hover:duration-100 hover:ease-linear rounded-full">
-            <div className="w-[152px] h-[152px]  bg-[#DB2E4D] px-6 rounded-full flex items-center justify-center">
-              <div className="bg-gray-300 w-[152px] h-[105px] rounded-full flex items-center justify-center">
+            <div className="w-[152px] h-[152px]  bg-[#DB2E4D] px-6 rounded-full flex  items-center justify-center shadow-[inset_0_-6px_0_#9D1634]  ">
+              <div className="bg-gray-300 w-[152px] h-[105px] rounded-full flex items-center justify-center  shadow-[inset_0_6px_0_0_#BABFD4]">
                 <img src="/icon-rock.svg" />
               </div>
             </div>
